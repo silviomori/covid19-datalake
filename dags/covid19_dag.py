153585631c10586e111be185392e72cb2e143983
Silvio Mori Neto <silvio@technomori.com>
2020-05-28 11:57:51 -0300

    feat: Spin down the EMR cluster

--- dags/covid19_dag.py
+++ dags/covid19_dag.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.contrib.operators.emr_create_job_flow_operator import EmrCreateJobFlowOperator
+from airflow.contrib.operators.emr_terminate_job_flow_operator import EmrTerminateJobFlowOperator
 
 from covid19_dag_settings import default_args, emr_settings
 from operators.termination_operator import TerminationOperator
@@ -24,7 +25,15 @@ create_emr_cluster_task = EmrCreateJobFlowOperator(
     dag=dag
 )
 
+terminate_emr_cluster_task = EmrTerminateJobFlowOperator(
+    task_id='terminate_emr_cluster',
+    job_flow_id="{{ task_instance.xcom_pull('create_emr_cluster', key='return_value') }}",
+    trigger_rule="all_done",
+    dag=dag
+)
+
 termination_task = TerminationOperator(task_id='termination', dag=dag)
 
 starting_point >> create_emr_cluster_task
-create_emr_cluster_task >> termination_task
+create_emr_cluster_task >> terminate_emr_cluster_task
+terminate_emr_cluster_task >> termination_task
